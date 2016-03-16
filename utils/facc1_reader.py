c38ff1efc8ce2af82a32a5ef61b42e5f4901eeff
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2016-03-16 21:51:48 +0900

    fix: FACC1Reader 버그 수정

--- utils/facc1_reader.py
+++ utils/facc1_reader.py
@@ -2,17 +2,25 @@
 import tarfile
 from tarfile import TarInfo
 import pandas as pd
+import os
+import logging
 
 class FACC1Reader(object):
     COLUMNS = ['trec_id', 'encoding', 'entity', 'start', 'end', 'posterior', 'posterior_context_only', 'freebase_tag']
     
     def __init__(self, fpath):
         self.fpath = fpath
+        logging.basicConfig(filename='facc1.log', level=logging.INFO)
     
     def __iter__(self):
         with tarfile.open(self.fpath, 'r:gz') as tar:
             for member in tar.getmembers():
-                if member.name.endswith('.tsv'):
+                fname = os.path.basename(member.name)
+                if fname.endswith('.tsv'):
                     f = tar.extractfile(member)
-                    df = pd.read_csv(f, sep='\t', header=None, names=self.COLUMNS)
-                    yield (member.name.replace('.tsv', ''), df)
+                    try:
+                        df = pd.read_csv(f, sep='\t', header=None, names=self.COLUMNS)
+                        logging.info('Finished: %s (%s)' % (fname, self.fpath))
+                        yield (fname.replace('.tsv', ''), df)
+                    except:
+                        logging.warning('Error: %s (%s)' % (fname, self.fpath))
