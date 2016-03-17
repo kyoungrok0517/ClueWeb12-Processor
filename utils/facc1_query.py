855dceeb3946d8cc69747758b6f64d8598f96212
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2016-03-17 12:43:55 +0900

    feat: FACC1Query 구현 개선

--- utils/facc1_query.py
+++ utils/facc1_query.py
@@ -1,9 +1,16 @@
 # coding: utf-8
+from blaze import Data
+from odo import odo
+from pandas import DataFrame
+import psycopg2
+
 class FACC1Query(object):
-    def __init__(self, uri='postgresql://postgres:Rudfhr88!@server.kyoungrok.com/facc1::clueweb12_00'):
-        self.uri = uri
+    SERVER_URI = 'postgresql://facc1:Rudfhr88!@server.kyoungrok.com/facc1::{0}'
+    
+    def __init__(self, collection):
+        self.uri = self.SERVER_URI.format(collection) # clueweb12_00
         self.facc1 = Data(self.uri)
         
-    def get_entities(self, trec_id):
+    def __call__(self, trec_id):
         df = odo(self.facc1[self.facc1.trec_id == trec_id], DataFrame)
         return [tuple(r) for r in df.to_records(index=False)]
