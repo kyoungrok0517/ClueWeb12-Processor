622cd8ba9a33f59188586876e69ca42183fbcbc8
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2016-03-22 12:09:34 +0900

    feat: 'utf-8'로 디코딩한 바이트 스트링의 길이가 content-length와 일치함을 확인

--- utils/clueweb_reader.py
+++ utils/clueweb_reader.py
@@ -27,9 +27,12 @@ class ClueWebReader(object):
     HEADER_OFFSET = 157
     HEADER_DELIM = '\r\n\r\n'
 
-    def __init__(self, fpath):
+    def __init__(self, fpaths):
         # target file path
-        self.fpath = fpath
+        if not isinstance(fpaths, list):
+            fpaths = [fpaths]
+        
+        self.fpaths = fpaths
         
     def __getitem__(self, key):
         return self.record_tuples[key]
@@ -38,17 +41,19 @@ class ClueWebReader(object):
         return len(self.record_tuples)
                 
     def __iter__(self):
-        f = warc.open(self.fpath)
-        for record in f:
-            if record.type == 'response':
-                try:
-                    content = record.payload.read().split(self.HEADER_DELIM)[1:]
-                    content = ''.join(content)
-                    content = unicode(content, encoding='utf-8', errors='replace')
-                    yield (record.header['WARC-TREC-ID'], content)
-                except Exception as e:
-                    print(e)
-        f.close()
+        for fpath in self.fpaths:
+            f = warc.open(fpath)
+            for record in f:
+                if record.type == 'response':
+                    try:
+                        # content = record.payload.read().split(self.HEADER_DELIM)[1:]
+                        # content = ''.join(content)
+                        # content = unicode(content, encoding='utf-8', errors='replace')
+                        content = unicode(record.payload.read(), encoding='utf-8', errors='replace')
+                        yield (record.header['WARC-TREC-ID'], record.header['WARC-Data-Type'], content)
+                    except Exception as e:
+                        print(e)
+            f.close()
         
     def _get_content_length(self, record):
         string = record.payload.read()
