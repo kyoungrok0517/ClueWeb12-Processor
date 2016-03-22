a0bb1c67ff706179a4667bfec29ff646ac1b1833
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2016-03-23 00:49:08 +0900

    feat: ClueWebTagger 1차 실험 성공
    일부 경우 1바이트씩 어긋나는 경우가 있는데, 이러한 에러 처리를 어떻게 할 것인지가 관건

--- utils/facc1_query.py
+++ utils/facc1_query.py
@@ -5,12 +5,13 @@ from pandas import DataFrame
 import psycopg2
 
 class FACC1Query(object):
-    SERVER_URI = 'postgresql://facc1:Rudfhr88!@server.kyoungrok.com/facc1::{0}'
+    SERVER_URI = 'postgresql://postgres:Rudfhr88!@server.kyoungrok.com/facc1::{0}'
+    DSHAPE = 'var * {trec_id: string, encoding: string, entity: ?string, start: int64, end: int64, posterior: float64, posterior_context_only: float64, tag: string}'
     
-    def __init__(self, collection):
+    def __init__(self, collection='clueweb12_00'):
         self.uri = self.SERVER_URI.format(collection) # ex: clueweb12_00
         self.facc1 = Data(self.uri)
         
     def __call__(self, trec_id):
-        df = odo(self.facc1[self.facc1.trec_id == trec_id], DataFrame)
+        df = odo(self.facc1[self.facc1.trec_id == trec_id], DataFrame, dshape=self.DSHAPE).sort_values('start')
         return [tuple(r) for r in df.to_records(index=False)]
